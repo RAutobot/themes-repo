458c6a2b6139623d25aadc5f0091bce19881d9ca
RAutobot <245641318+RAutobot@users.noreply.github.com>
2025-11-24 12:36:39 +0700

    Initial commit

---
+++ generate-data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir public
+jq -r '
+    to_entries[] | .key as $author | .value |
+    to_entries[] | .key as $repo | .value[] as $path |
+    "\($author) \($repo) \($path)"
+' themes | xargs -n 3 -P 20 bash -c '
+    author="$1"
+    repo="$2"
+    path="$3"
+
+    url="https://cdn.jsdelivr.net/gh/$author/$repo/$path.json"
+    repoUrl="https://github.com/$author/${repo%%@*}"
+    filename="${path##*/}.json"
+
+    curl -s "$url" | jq -c --arg url "$url" --arg repoUrl "$repoUrl" --arg filename "$filename" '\''.manifest | { name, version, author, url: $url, repoUrl: $repoUrl, filename: $filename }'\''
+' _ | jq -s 'sort_by(.name)' > public/data.json
